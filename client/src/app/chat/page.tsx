569bfa597cade1ddd61647d763d899bf666dd1e3
baazithh <mabasithpullambalavan@gmail.com>
2026-04-07 16:42:32 +0530

    updated css

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -25,9 +25,9 @@ export default function ChatPage() {
 
   return (
     <main className="min-h-screen relative flex flex-col pt-24">
-      {/* HUD Background Elements */}
-      <div className="fixed inset-0 pointer-events-none -z-10 bg-[#05050a]" />
-      <div className="fixed top-0 left-1/2 -translate-x-1/2 w-full h-[600px] bg-[radial-gradient(circle_at_50%_0%,rgba(6,182,212,0.15)_0%,transparent_70%)] pointer-events-none -z-10" />
+      {/* HUD Background Elements - Light */}
+      <div className="fixed inset-0 pointer-events-none -z-10 bg-[#f8fafc]" />
+      <div className="fixed top-0 left-1/2 -translate-x-1/2 w-full h-[600px] bg-[radial-gradient(circle_at_50%_0%,rgba(6,182,212,0.08)_0%,transparent_70%)] pointer-events-none -z-10" />
       
       <NavBar />
       <div className="relative z-10 flex-1">
